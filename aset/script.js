844753c508b728f299227939d209c6027fb3bea0
DitaPutraPratama <ditaputra1296@gmail.com>
2023-06-04 23:49:10 +0700

    format document

--- aset/script.js
+++ aset/script.js
@@ -1,7 +1,7 @@
 // atas
 var button = document.getElementById("tombolAtas");
 
-    window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     if (window.scrollY > 0) {
         button.style.display = "block";
     } else {
@@ -19,28 +19,28 @@ function darkMode() {
 
 // form
 const scriptURL =
-            'https://script.google.com/macros/s/AKfycbxditIiN8glhmMKZD3e0bKZICmk116jyYcS51PAfR3PynPdtRcdy4I5B1CdiqMZ0FPYmQ/exec'
-        const form = document.forms['submit-to-google-sheet']
-        const btnKirim = document.querySelector('.btn-kirim');
-        const btnLoading = document.querySelector('.btn-loading');
-        const alert = document.querySelector('.alert');
+    "https://script.google.com/macros/s/AKfycbxditIiN8glhmMKZD3e0bKZICmk116jyYcS51PAfR3PynPdtRcdy4I5B1CdiqMZ0FPYmQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const alert = document.querySelector(".alert");
 
-        form.addEventListener('submit', e => {
-            e.preventDefault()
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-            btnLoading.classList.toggle('d-none');
-            btnKirim.classList.toggle('d-none');
-            fetch(scriptURL, {
-                    method: 'POST',
-                    body: new FormData(form)
-                })
-                .then(response => {
-                    btnLoading.classList.toggle('d-none');
-                    btnKirim.classList.toggle('d-none');
-                    alert.classList.toggle('d-none');
-                    form.reset();
-                    console.log('Success!', response)
-                })
-                .catch(error => console.error('Error!', error.message))
+    btnLoading.classList.toggle("d-none");
+    btnKirim.classList.toggle("d-none");
+    fetch(scriptURL, {
+        method: "POST",
+        body: new FormData(form),
+    })
+        .then((response) => {
+            btnLoading.classList.toggle("d-none");
+            btnKirim.classList.toggle("d-none");
+            alert.classList.toggle("d-none");
+            form.reset();
+            console.log("Success!", response);
         })
+        .catch((error) => console.error("Error!", error.message));
+});
 // akhir form
